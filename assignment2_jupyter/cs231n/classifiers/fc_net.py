63aefa5d9d8ef4799050447e487505d1a83b65c0
fmozo <fm.login1@gmail.com>
2020-10-28 18:52:05 -0300

    update 28/10 6:52

--- assignment2_jupyter/cs231n/classifiers/fc_net.py
+++ assignment2_jupyter/cs231n/classifiers/fc_net.py
@@ -55,7 +55,11 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        self.params['W1'] = np.random.randn(input_dim, hidden_dim)*weight_scale
+        self.params['b1'] = np.zeros(hidden_dim)
+
+        self.params['W2'] = np.random.randn(hidden_dim, num_classes)*weight_scale
+        self.params['b2'] = np.zeros(num_classes)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -88,7 +92,15 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        # unpack de los pesos y los biases
+        W1 = self.params['W1']
+        b1 = self.params['b1']
+        W2 = self.params['W2']
+        b2 = self.params['b2']
+
+        out1, cache1 = affine_relu_forward(X, W1, b1)
+
+        scores, cache2 = affine_forward(out1, W2, b2)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -112,7 +124,21 @@ class TwoLayerNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        loss, dout = softmax_loss(scores, y)
+
+        # agrego regularizacion L2
+        loss += 0.5 * self.reg * (np.sum(W1*W1) + np.sum(W2*W2))
+
+        dscores, dW2, db2 = affine_backward(dout, cache2)
+
+        dX, dW1, db1 = affine_relu_backward(dscores, cache1)
+
+
+        grads['W1'] = dW1 + self.reg * W1
+        grads['b1'] = db1
+
+        grads['W2'] = dW2 + self.reg * W2
+        grads['b2'] = db2
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
